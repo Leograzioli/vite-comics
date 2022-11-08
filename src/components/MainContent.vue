0b313a7be5356cd8125ea2ae7ee013e93ecfb279
Leograzioli <leograzioli@gmail.com>
2022-11-08 14:14:31 +0100

    main

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -10,9 +10,19 @@ export default {
 </script>
 
 <template>
-    <h2>main</h2>
+    <main>
+        <div class="container">
+            <h2>-- content hoes here -- </h2>
+        </div>
+    </main>
 </template>
 
 <style lang="scss" scoped>
+main {
 
+    // debug
+    background-color: black;
+    color: white;
+    padding: 3rem 0;
+}
 </style>
