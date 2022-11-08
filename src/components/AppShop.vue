7c063a470200abfcba23849fd74e03b6e15b2b39
Leograzioli <leograzioli@gmail.com>
2022-11-08 15:36:04 +0100

    fatto shoping section

--- src/components/AppShop.vue
+++ src/components/AppShop.vue
@@ -11,13 +11,58 @@ export default {
 
 <template>
     <section id="shop">
-        <div class="container">shop</div>
+        <div class="container flex">
+            <div class="col">
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <p>DIGITAL COMICS</p>
+            </div>
+            <div class="col">
+                <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                <p>DC MERCHANDISE</p>
+            </div>
+            <div class="col">
+                <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                <p>SUBSCRIPTION</p>
+            </div>
+            <div class="col">
+                <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                <p>COMIC SHOP LOCATOR</p>
+            </div>
+            <div class="col">
+                <img class="visa" src="../assets/img/buy-dc-power-visa.svg" alt="">
+                <p>DC POWER VISA</p>
+            </div>
+        </div>
     </section>
 </template>
 
 <style lang="scss" scoped>
 #shop {
     background-color: blue;
-    padding: 3rem 0;
+    padding: 3rem 0rem;
+
+    .container {
+
+        .col {
+            width: calc(100% / 5);
+
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            p {
+                margin-left: 1rem;
+            }
+
+            img {
+                height: 60px;
+            }
+
+            .visa {
+                height: 40px;
+            }
+        }
+    }
+
 }
 </style>
