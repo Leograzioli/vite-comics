554cfa0fff6b14c372081fe3f337f91cc1b5a59b
Leograzioli <leograzioli@gmail.com>
2022-11-08 13:34:09 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import { def } from '@vue/shared';
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<template>ciao</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
