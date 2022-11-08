00b2201baca4c28b17603870398addf3b04d8460
Leograzioli <leograzioli@gmail.com>
2022-11-08 13:54:51 +0100

    add dati della header nav bar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,14 +3,67 @@ export default {
     name: "AppHeader",
     data() {
         return {
-
+            links: [
+                {
+                    name: 'CHARACTERS',
+                    href: '/CHARACTERS',
+                    active: false
+                },
+                {
+                    name: 'COMICS',
+                    href: '/COMICS',
+                    active: false
+                },
+                {
+                    name: 'MOVIES',
+                    href: '/MOVIES',
+                    active: false
+                },
+                {
+                    name: 'TV',
+                    href: '/TV',
+                    active: false
+                },
+                {
+                    name: 'GAMES',
+                    href: '/GAMES',
+                    active: false
+                },
+                {
+                    name: 'COLLECTIBLES',
+                    href: '/COLLECTIBLES',
+                    active: false
+                },
+                {
+                    name: 'VIDEOS',
+                    href: '/VIDEOS',
+                    active: false
+                },
+                {
+                    name: 'FANS',
+                    href: '/FANS',
+                    active: false
+                },
+                {
+                    name: 'NEWS',
+                    href: '/NEWS',
+                    active: false
+                },
+                {
+                    name: 'SHOP',
+                    href: '/SHOP',
+                    active: false
+                },
+            ],
         }
     }
 }
 </script>
 
 <template>
-    <h2>header</h2>
+    <header>
+
+    </header>
 </template>
 
 <style lang="scss" scoped>
