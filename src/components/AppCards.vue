3154ab4efb13d3d9062dcf1d079da7f81816f1d3
Leograzioli <leograzioli@gmail.com>
2022-11-09 14:23:37 +0100

    agiunto componente stylizzato

---
+++ src/components/AppCards.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    name: 'AppCards',
+    props: {
+        img: String,
+        title: String
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="col">
+        <img :src="img" :alt="title">
+        <p>{{ title }}</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.col {
+    width: calc(100% /6 - 30px);
+    margin: 30px 15px;
+    max-height: 190px;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: top;
+    }
+
+    p {
+        margin-top: 10px;
+        color: white;
+    }
+}
+</style>
